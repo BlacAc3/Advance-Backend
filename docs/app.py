2b715df6c021017c71a0a1ea90fb6ce8ac46cc32
blacac3 <aceezeala@gmail.com>
2025-07-17 00:23:25 +0100

    documentation complete

---
+++ docs/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    "/swagger",
+    '/swagger-docs.json',
+    config={
+        'app_name': "Advance API",
+        "layout": "BaseLayout",
+        "docExpansion": "none"
+    },
+)
+app.register_blueprint(swaggerui_blueprint)
+
+
+@app.route('/swagger-docs.json')
+def get_swagger():
+    try:
+        return send_from_directory(os.path.dirname(app.root_path), 'swagger-output.json')
+    except FileNotFoundError:
+        return jsonify({"error": "swagger-output.json not found"}), 404
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
