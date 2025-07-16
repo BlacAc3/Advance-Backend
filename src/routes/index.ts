9423aae079ed0836f1da46febb749a79b6eabc98
blacac3 <aceezeala@gmail.com>
2025-07-16 10:18:15 +0100

    change route style

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,34 +10,9 @@ import path from "path";
 export const setupRoutes = (app: Express) => {
   // const apiPrefix = `/api/${process.env.API_VERSION || "v1"}`;
   // Serve Swagger documentation
-  app.get("/api-docs", (req, res) => {
-    res.setHeader(
-      "Content-Security-Policy",
-      "default-src 'self'; " +
-        "script-src 'self' https://cdn.jsdelivr.net; " + // Allow scripts from self and cdn.jsdelivr.net
-        "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline';" + // Allow styles from self and cdn.jsdelivr.net, and inline styles for swagger-ui.css
-        "img-src 'self' data:;", // Often needed for swagger-ui logos, etc.
-    );
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>My API Documentation</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.11.0/swagger-ui.css">
-      </head>
-      <body>
-        <div id="swagger-ui"></div>
-        <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.11.0/swagger-ui-bundle.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.11.0/swagger-ui-standalone-preset.js"></script>
-        <script src="public/swagger-init.js"></script> </body>
-        </body>
-      </html>
-    `);
-  });
-
   // An endpoint to serve your swagger.json or openapi.yaml
-  app.get("/swagger.json", (req, res) => {
-    res.sendFile(path.join(__dirname, "swagger-output.json")); // Or res.json(yourSwaggerSpecObject);
+  app.get("/swagger", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/swagger-output.json")); // Or res.json(yourSwaggerSpecObject);
   });
   // API routes
   app.use(`/api/v1/auth`, authRoutes);
